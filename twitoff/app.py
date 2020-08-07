f058d1dd540400b5c396a18008043e33f8221e93
Yuanjin Ren <Tinaren@Darrens-MBP.fios-router.home>
2020-08-06 21:44:02 -0400

    Assignment1

---
+++ twitoff/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from .db_model import db, User
+
+def create_app():
+    '''Create and configure an instance of the Flask application'''
+
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/Tinaren/Desktop/LambdaSchool/Unit3/WebApp/twitoff.sqlite"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+
+    @app.route('/')
+    def root():
+        return render_template('base.html', title='Home', users=User.query.all())
+
+    return app
