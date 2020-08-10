56e4c7f1e88d04e55b7f2ce882b77265ac30cef7
Yuanjin Ren <Tinaren@Darrens-MBP.fios-router.home>
2020-08-10 19:38:33 -0400

    Users can be added sucessfully

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .db_model import db, User
-#from .twitter import add_user_tweepy
+from .twitter import add_user_tweepy
 
 def create_app():
     '''Create and configure an instance of the Flask application'''
@@ -14,19 +14,19 @@ def create_app():
     def root():
         return render_template('base.html', title='Home', users=User.query.all())
 
-    # @app.route('/user', method=['POST'])
-    # @app.route('/user/<name>', methods=['GET'])
-    # def user(name=None, message=''):
-    #     name = name or request.values['user_name']
-    #     try:
-    #         if request.method =='POST':
-    #             add_user_tweepy(name)
-    #             message = "User {} successfully added!".format(name)
-    #         tweets = User.query.filter(User.username == name).one().tweet
-    #     except Exception as e:
-    #         message = "Error adding {}: {}".format(name, e)
-    #         tweets = []
-    #     return render_template('user.html',title = name, tweets=tweets, message=message)
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method =='POST':
+                add_user_tweepy(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.username == name).one().tweet
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html',title = name, tweets=tweets, message=message)
     return app
 
 
--- twitoff/db_model.py
+++ twitoff/db_model.py
@@ -4,11 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
     follower_count = db.Column(db.Integer, nullable=False)
     # Tweet IDs are ordinal ints, so we can fetch most recent tweets
-    #newest_tweet_id = db.Column(db.BigInteger, nullable=False)
+    newest_tweet_id = db.Column(db.BigInteger)
+    #tweets = db.relationship('Tweet', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -16,8 +17,8 @@ class User(db.Model):
 
 class Tweet(db.Model):
     id = db.Column(db.BigInteger, primary_key=True)
-    text = db.Column(db.Unicode(300))
-    # embedding = db.Column(db.PickleType, nullable=False)
+    text = db.Column(db.String(300))
+    embedding = db.Column(db.PickleType, nullable=False)
     user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('tweet', lazy=True))
 
@@ -29,9 +30,9 @@ class Tweet(db.Model):
 #     db.create_all()
 
 #     u1 = User(username = 'elonmusk', follower_count = 37000000)
-#     user2 = User(username = 'ladygaga', follower_count = 80000000)
-#     user3 = User(username = 'ellen', follower_count = 79900000)
-#     user4 = User(username = 'youtube', follower_count = 72200000)
+#     u2 = User(username = 'ladygaga', follower_count = 80000000)
+#     u3 = User(username = 'ellen', follower_count = 79900000)
+#     u4 = User(username = 'youtube', follower_count = 72200000)
 #     u5 = User(username = 'britney', follower_count = 56000000)
 #     u6 = User(username = 'demi', follower_count = 55000000)
 
